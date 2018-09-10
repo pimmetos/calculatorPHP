08ab6d3e39a209bb464c46fa25f0a2b8093d9c65
Pim <pimmetos@hotmail.com>
2018-09-10 19:19:02 +0200

    string

---
+++ calculator.php
@@ -0,0 +1,39 @@
+<?php
+
+$myName = "Pim van Berlo";// assign value to variable
+
+$number1 = 456; // integer
+
+$number4 = 6.34 // decimal
+
+$waar = true; // boolean
+$onwaar = false; // boolean
+
+$array = ["lol","69","HEHE xD","whytho"] // array
+
+?>
+
+
+<!doctype <!DOCTYPE html>
+<!-- HTML comment -->
+
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Calculator</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+
+<body>
+    <?php
+
+        echo $myName;
+
+    ?>
+</body>
+
+</html>
